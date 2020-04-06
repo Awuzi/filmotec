7d9cb461de18dca76efccca13cb9d1cdd12f5017
Yahia LAMRI <yahia.lamri@outlook.fr>
2020-04-06 21:20:40 +0200

    first release done

--- models/User.js
+++ models/User.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
     username: String,
     password: String,
     name: String
 });
-
-UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', UserSchema);
